9c6ae32ba0e5cb3e724f92e5726aea207d72f89b
Draco <draco.coder@gmail.com>
2022-07-09 17:15:47 +0800

    feat: 自动更新  glinet-ax1800.yml 配置文件

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,23 +1,44 @@
----
+const fs = require('fs');
+const path = require('path');
+const exec = require('child_process').execSync;
+
+
+/**
+ * 生成 feeds 配置
+ * @param {*} name
+ * @param {*} uri
+ * @param {*} branch
+ * @returns
+ */
+ const GenerateFeedsConfig = (name, uri, branch) => {
+  exec(`git clone --depth=1 ${uri} -b ${branch} ${name}`);
+  const revision = exec(`cd ${name} && git log -1 --pretty=%H`).toString().trim();
+  exec(`cd ..`);
+  exec(`rm -rf ${name}`);
+  const config = `  - name: ${name}
+    uri: ${uri}
+    revision: ${revision}`;
+  return config;
+}
+
+/**
+ * 生成 feeds 配置文件
+ */
+const GenerateYml = () => {
+  // 读取 feeds 配置文件
+  const feedsPath = path.resolve(process.cwd(), 'scripts', 'feeds.json')
+  const feeds = require(feedsPath);
+  // 生成 feeds 配置
+  const config = feeds.map(item => GenerateFeedsConfig(item.name, item.uri, item.branch)).join('\n');
+  // 生成配置文件路径
+  const filePath = path.resolve(process.cwd(), 'glinet-ax1800.yml');
+  // 写入配置文件
+  fs.writeFileSync(filePath, `---
 profile: glinet_ax1800
 description: Build image for the GL.iNET AX1800
 image: bin/targets/ipq807x/ipq60xx/openwrt-ipq807x-glinet_ax1800-squashfs-sysupgrade.tar
 feeds:
-  - name: immortalwrt_packages
-    uri: https://github.com/immortalwrt/packages.git
-    revision: bf04e7076f6a538201f0b77f65eac597cea66609
-  - name: immortalwrt_luci
-    uri: https://github.com/immortalwrt/luci.git
-    revision: ddf45cf3b5a82868cec10fcc2cddbbb1958340ef
-  - name: lienol
-    uri: https://github.com/Lienol/openwrt-package.git
-    revision: d607aef7267d3e3c9c13a74d3f40fbbbfd4020e1
-  - name: kenzok8
-    uri: https://github.com/kenzok8/openwrt-packages.git
-    revision: 5bfa9f92ac54bc04fe3589daab7fb25ee86cbdba
-  - name: small
-    uri: https://github.com/kenzok8/small.git
-    revision: 7025b3265b7aaef4195eb80e7ea4c110a1fbb0d5
+${config}
 
 include:
   - target_wlan_ap-gl-ax1800-common-5-4
@@ -74,4 +95,7 @@ packages:
   # 支持计划重启
   - luci-app-autoreboot
   # 京东签到服务
-  - luci-app-jd-dailybonus
+  - luci-app-jd-dailybonus`);
+}
+
+GenerateYml();
