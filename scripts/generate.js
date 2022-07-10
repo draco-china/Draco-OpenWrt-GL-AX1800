e34bf1aa43cd3ec8b95e6bee3ec88c94b1165a75
Draco <draco.coder@gmail.com>
2022-07-10 09:47:58 +0800

    feat: 支持axt1800

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const exec = require('child_process').execSync;
+const model = process.argv[process.argv.length - 1];
 
 
 /**
@@ -34,17 +35,17 @@ const GenerateYml = () => {
   const packagesPath = path.resolve(process.cwd(), 'scripts', 'packages.yml')
   const packages = fs.readFileSync(packagesPath, 'utf8');
   // 生成配置文件路径
-  const filePath = path.resolve(process.cwd(), 'glinet-ax1800.yml');
+  const filePath = path.resolve(process.cwd(), `glinet-${model}.yml`);
   // 写入配置文件
   fs.writeFileSync(filePath, `---
-profile: glinet_ax1800
-description: Build image for the GL.iNET AX1800
-image: bin/targets/ipq807x/ipq60xx/openwrt-ipq807x-glinet_ax1800-squashfs-sysupgrade.tar
+profile: glinet_${model}
+description: Build image for the GL.iNET ${model.toLocaleUpperCase()}
+image: bin/targets/ipq807x/ipq60xx/openwrt-ipq807x-glinet_${model}-squashfs-sysupgrade.tar
 feeds:
 ${config}
 
 include:
-  - target_wlan_ap-gl-ax1800-common-5-4
+  - target_wlan_ap-gl-${model}-common-5-4
 
 ${packages}`);
 }
