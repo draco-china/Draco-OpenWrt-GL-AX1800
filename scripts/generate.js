c953e527a0838c88215526e537dd595f827474ec
Draco <draco.coder@gmail.com>
2022-07-10 18:13:38 +0800

    feat: 优化生成的编译文件

--- scripts/generate.js
+++ scripts/generate.js
@@ -28,6 +28,8 @@ const GenerateYml = () => {
   try {
     exec(`npm install js-yaml`);
     const yaml = require('js-yaml');
+    exec(`npm install lodash`);
+    const _ = require('lodash');
 
     const glInfraBuilder = path.resolve(process.cwd(), 'gl-infra-builder')
     exec(`git clone --depth=1 https://github.com/gl-inet/gl-infra-builder -b main ${glInfraBuilder}`);
@@ -35,30 +37,43 @@ const GenerateYml = () => {
     const ax1800Config = yaml.load(fs.readFileSync(`${glInfraBuilder}/profiles/target_wlan_ap-gl-ax1800-5-4.yml`, 'utf8'));
     const axt1800Config = yaml.load(fs.readFileSync(`${glInfraBuilder}/profiles/target_wlan_ap-gl-axt1800-5-4.yml`, 'utf8'));
 
+    // 读取官方公共配置
+    const ax1800ConfigCommon = yaml.load(fs.readFileSync(`${glInfraBuilder}/profiles/${ax1800Config.include[0]}.yml`, 'utf8'));
+    const axt1800ConfigCommon = yaml.load(fs.readFileSync(`${glInfraBuilder}/profiles/${axt1800Config.include[0]}.yml`, 'utf8'));
+
+    // 移除 include 字段
+    ax1800Config.include = [];
+    axt1800Config.include = [];
+
+    // 合并官方配置
+    const ax1800ConfigMerge = _.merge(ax1800Config, ax1800ConfigCommon);
+    const axt1800ConfigMerge = _.merge(axt1800Config, axt1800ConfigCommon);
+
     // 读取 feeds 配置文件
     const feedsPath = path.resolve(process.cwd(), 'scripts', 'feeds.json')
     // 生成 feeds 配置
     const feeds = require(feedsPath).map(item => GenerateFeedsConfig(item.name, item.uri, item.branch));
 
     // 合并 feeds 配置
-    ax1800Config.feeds = ax1800Config.feeds ? ax1800Config.feeds.concat(packagesConfig.feeds) :feeds;
-    axt1800Config.feeds= axt1800Config.feeds ? axt1800Config.feeds.concat(packagesConfig.feeds) :feeds;
+    ax1800ConfigMerge.feeds = ax1800ConfigMerge.feeds ? ax1800ConfigMerge.feeds.concat(feeds) :feeds;
+    axt1800ConfigMerge.feeds= axt1800ConfigMerge.feeds ? axt1800ConfigMerge.feeds.concat(feeds) :feeds;
 
     // 读取 packages 配置文件
     const packagesPath = path.resolve(process.cwd(), 'scripts', 'packages.yml')
     const packagesConfig =  yaml.load(fs.readFileSync(packagesPath, 'utf8'));
 
     // 合并 packages 配置
-    ax1800Config.packages = ax1800Config.packages ? ax1800Config.packages.concat(packagesConfig.packages) :packagesConfig.packages;
-    axt1800Config.packages = axt1800Config.packages ? axt1800Config.packages.concat(packagesConfig.packages) :packagesConfig.packages;
+    ax1800ConfigMerge.packages = ax1800ConfigMerge.packages ? ax1800ConfigMerge.packages.concat(packagesConfig.packages) :packagesConfig.packages;
+    axt1800ConfigMerge.packages = axt1800ConfigMerge.packages ? axt1800ConfigMerge.packages.concat(packagesConfig.packages) :packagesConfig.packages;
 
     // 转换为 YAML
-    const ax1800ConfigYml = yaml.dump(ax1800Config, {
-      lineWidth: -1
+    const ax1800ConfigYml = yaml.dump(ax1800ConfigMerge, {
+      lineWidth: -1,
+      sortKeys: true
     });
-
-    const axt1800ConfigYml = yaml.dump(axt1800Config, {
-      lineWidth: -1
+    const axt1800ConfigYml = yaml.dump(axt1800ConfigMerge, {
+      lineWidth: -1,
+      sortKeys: true
     });
 
     // 生成配置文件路径
