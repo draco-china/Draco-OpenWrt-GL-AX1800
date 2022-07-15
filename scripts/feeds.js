1c35e5042d9ac5a83be7aee5da91c32358917d23
wifiway520 <54652920+wifiway520@users.noreply.github.com>
2022-07-15 21:52:48 +0800

    Update feeds.js

--- scripts/feeds.js
+++ scripts/feeds.js
@@ -1,22 +1,22 @@
 module.exports = [
   {
     // feed 名称
-    name: 'immortalwrt',
+   // name: 'immortalwrt',
     // feed 仓库
-    uri: 'https://github.com/immortalwrt/luci.git',
+   // uri: 'https://github.com/immortalwrt/luci.git',
     // feed 分支
-    branch: 'openwrt-21.02',
-  },
+  //  branch: 'openwrt-21.02',
+ // },
   // {
   //   name: 'lienol',
   //   uri: 'https://github.com/Lienol/openwrt-package.git',
   //   branch: 'main',
   // },
-  // {
-  //   name: 'kenzok8',
-  //   uri: 'https://github.com/kenzok8/openwrt-packages.git',
-  //   branch: 'master',
-  // },
+   {
+     name: 'kenzok8',
+     uri: 'https://github.com/kenzok8/openwrt-packages.git',
+     branch: 'master',
+   },
   {
     name: 'small',
     uri: 'https://github.com/kenzok8/small.git',
